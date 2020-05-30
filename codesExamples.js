3603987c28740a2387b4329a1a0a72210bc04cb8
whiteromka <58069286+whiteromka@users.noreply.github.com>
2020-05-30 19:25:27 +0300

    Update codesExamples.js

--- codesExamples.js
+++ codesExamples.js
@@ -1,5 +1,23 @@
+/** 1 */
 handleMarket(name) {
   const cars = this.state.cars.concat() //клонирование
   const car = cars.find(c => c.name === name)
   car.marked = !car.marked
 }
+
+
+/** 2 */
+titleChange(name) {
+  this.setState({title: name})
+}
+...
+<input 
+  onCange={(e)=>this.titleChange(e.target.value)}
+  value={this.state.title}
+/>
+    
+
+/** 3 */
+...
+<div>{this.props.children}</div>
+
